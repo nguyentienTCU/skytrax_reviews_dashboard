7d1f6ff6a276967be235f78ed8d8901344d29546
tiennguyenTCU <nguyentien3036@gmail.com>
2025-04-25 23:01:14 -0500

    add beautiful icons

--- src/components/DataSummary/DataSummary.tsx
+++ src/components/DataSummary/DataSummary.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { getDataSummary } from "@/lib/getData/getDataSummary";
 const DataSummary = async () => {
-  const { totalReviews, totalVerifiedReviews, totalAircraftModels, totalCountries } = await getDataSummary();
+  const {
+    totalReviews,
+    totalVerifiedReviews,
+    totalAircraftModels,
+    totalCountries,
+  } = await getDataSummary();
   return (
     <div className="card">
       <h2 className="text-xl font-bold mb-4 text-gray-700">Data Summary</h2>
@@ -10,10 +15,12 @@ const DataSummary = async () => {
           <div className="flex items-center justify-between">
             <div>
               <p className="text-sm text-gray-600">Total Reviews</p>
-              <h3 className="text-2xl font-bold text-blue-600">{totalReviews}</h3>
+              <h3 className="text-2xl font-bold text-blue-600">
+                {totalReviews}
+              </h3>
             </div>
             <div className="bg-blue-200 p-3 rounded-full">
-              <i className="fas fa-comment text-blue-600"></i>
+              <i className="fa-solid fa-comment text-blue-600"></i>
             </div>
           </div>
         </div>
@@ -21,10 +28,12 @@ const DataSummary = async () => {
           <div className="flex items-center justify-between">
             <div>
               <p className="text-sm text-gray-600">Verified Reviews</p>
-              <h3 className="text-2xl font-bold text-green-600">{totalVerifiedReviews}%</h3>
+              <h3 className="text-2xl font-bold text-green-600">
+                {totalVerifiedReviews}%
+              </h3>
             </div>
             <div className="bg-green-200 p-3 rounded-full">
-              <i className="fas fa-check-circle text-green-600"></i>
+              <i className="fa-solid fa-check-circle text-green-600"></i>
             </div>
           </div>
         </div>
@@ -32,10 +41,12 @@ const DataSummary = async () => {
           <div className="flex items-center justify-between">
             <div>
               <p className="text-sm text-gray-600">Airlines</p>
-              <h3 className="text-2xl font-bold text-purple-600">{totalAircraftModels}</h3>
+              <h3 className="text-2xl font-bold text-purple-600">
+                {totalAircraftModels}
+              </h3>
             </div>
             <div className="bg-purple-200 p-3 rounded-full">
-              <i className="fas fa-plane text-purple-600"></i>
+              <i className="fa-solid fa-plane text-purple-600"></i>
             </div>
           </div>
         </div>
@@ -43,10 +54,12 @@ const DataSummary = async () => {
           <div className="flex items-center justify-between">
             <div>
               <p className="text-sm text-gray-600">Countries</p>
-              <h3 className="text-2xl font-bold text-yellow-600">{totalCountries}</h3>
+              <h3 className="text-2xl font-bold text-yellow-600">
+                {totalCountries}
+              </h3>
             </div>
             <div className="bg-yellow-200 p-3 rounded-full">
-              <i className="fas fa-globe text-yellow-600"></i>
+              <i className="fa-solid fa-globe text-yellow-600"></i>
             </div>
           </div>
         </div>
