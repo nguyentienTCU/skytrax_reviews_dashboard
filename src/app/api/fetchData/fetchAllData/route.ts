98f33ee803287e2173fab961d91d05062354c017
Duc Le <ducleanh2005@gmail.com>
2025-08-06 11:07:24 -0500

    remove unused api endpoint

--- src/app/api/fetchData/fetchAllData/route.ts
+++
@@ -1,7 +0,0 @@
-import { getChartData } from "@/lib/csv-parser";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const data = await getChartData("reviews.csv");
-  return NextResponse.json(data);
-}
