3506c67a3d4c890b23a7958a54607f36a9ce92e1
Duc Le <ducleanh2005@gmail.com>
2025-10-15 16:55:16 -0500

    Update UI

--- frontend/src/components/custom-ui/Header.tsx
+++ frontend/src/components/custom-ui/Header.tsx
@@ -1,26 +1,34 @@
 "use client";
 
+import Image from "next/image";
 import React from "react";
 
 const Header = () => {
+  return (
+    <div className="card relative min-h-[110px] h-[20vh] md:h-[18vh] flex items-center justify-center bg-gradient-to-r from-blue-600 to-purple-600 dark:from-blue-800 dark:to-purple-800 text-white">
+      <div className="flex items-center animate-pulse">
+        {/* --- LOGO --- */}
+        <div className="mr-4">
+          <Image
+            src="/web_logo.png"
+            alt="Flightly logo"
+            width={100}
+            height={100}
+            priority
+            className="h-12 w-12 md:h-14 md:w-14 select-none"
+          />
+        </div>
 
-	return (
-		<div className="card relative min-h-[110px] h-[20vh] md:h-[18vh] flex items-center justify-center bg-gradient-to-r from-blue-600 to-purple-600 dark:from-blue-800 dark:to-purple-800 text-white">
-			<div className="flex items-center animate-pulse">
-				<div>
-					<i className="fas fa-plane-departure text-4xl mr-4"></i>
-				</div>
-				<div>
-					<h1 className="text-3xl font-bold">
-						British Airways Flight Reviews Dashboard
-					</h1>
-					<p className="text-lg mt-2">
-						Interactive analysis of customer flight experiences
-					</p>
-				</div>
-			</div>
-		</div>
-	);
+        {/* --- TEXT --- */}
+        <div className="text-center md:text-left">
+          <h1 className="text-3xl font-bold">Flightly Dashboard</h1>
+          <p className="text-lg mt-2">
+            Interactive analysis of customer flight experiences
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
