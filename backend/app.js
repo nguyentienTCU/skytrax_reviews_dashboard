780e54a0d96bdbfc777d7e59fc36edd77d1b8119
Duc Le <ducleanh2005@gmail.com>
2025-10-15 18:55:22 -0500

    Fix port for dev env

--- backend/app.js
+++ backend/app.js
@@ -41,8 +41,7 @@ app.use((err, _req, res, _next) => {
   res.status(err.status || 500).json({ error: "Internal Server Error" });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT} (${isProd ? "prod" : "dev"})`)
-);
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server running on port ${process.env.PORT || 5000}`);
+});
 
